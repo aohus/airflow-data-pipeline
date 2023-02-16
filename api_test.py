7ac051961ff74a9a7457890f240ea9401d1a73ae
유수화 <aohus@laivdata.com>
2023-02-16 11:05:46 +0900

    first commit

---
+++ api_test.py
@@ -0,0 +1,16 @@
+import requests
+import json
+from datetime import datetime
+
+lat=37.5642135
+lon=127.0016985
+part='hourly,minutely'
+API_key='2d61882f7e74a4787667e1de1e8004b1'
+weather_api_url=f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API_key}&units=metric'
+
+f = requests.get(weather_api_url).json()
+l=[]
+for day in f['daily']:
+	l.append(tuple([datetime.fromtimestamp(day['dt']).strftime('%Y-%m-%d'), day['temp']['day'], day['temp']['min'], day['temp']['max']]))
+print(l)
+#query='''INSERT INTO dbtnghk528.weather_forcast VALUES ('Andrew', 'Male');'''
